ae7be4c19416cc722d2637d6c97c15809744dda0
hesenovzamin <hesenovzamin@gmail.com>
2018-08-25 13:39:12 +0400

    Navbar

--- js/scpit.js
+++ js/scpit.js
@@ -1,13 +1,71 @@
+// $(".icon2").click(function(){
+//     $(".icon2").css("background","black")
+//     console.log("aaaaa");
+// })
 
-var sayi = 25;
-$(window).resize(function(){
-    if(window.innerWidth<768)
-    {
+// $("i").click(function(){
+//     $("i").css("color","black");
+// });
+
+var nav = true;
+$(document).ready(function(){
+    $('.Icon2').click(function(){
+      
+      if(nav == true){
+        $('.Icon2').css("color","white");
+        $(this).css("color","black"); 
+        nav = false;
+      }
+      else{
+        $(this).css("color","white");
+        nav = true;
+      }
+    });
+});
+
+var Hide = true;
+$(".Left").click(function(){
+   if(Hide==true){
+    $(".logo").css("width","50px");
+    $(this).css("margin-left","60px");
+    console.log("asd");
+    Hide=false;
+   }
+   else{
+    $(".logo").css("width","250px");
+    $(this).css("margin-left","265px");
+    Hide=true;
+   }
+  
+})
+
+// $('.Navbar').click(function(){
+//     if(nav ==false)
+//     {
+//       $('i').css("color","white");
+//       console.log("");
+//       nav = true;
+//     }
+//   });
+
+
+// $(function(){
+//     $("i").click(function(){
+//         $(this).css("color","black");
+//     },function(){
+
+//     });
+
+// });
+// var sayi = 25;
+// $(window).resize(function(){
+//     if(window.innerWidth<768)
+//     {
        
         
-    }
-    else{
+//     }
+//     else{
      
-    }
-    console.log(window.innerWidth)
-    })
+//     }
+//     console.log(window.innerWidth)
+//     })
